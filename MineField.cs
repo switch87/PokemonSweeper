a259255e9ebb075287a617086e6acedb49f4a652
Gert <gert@gepe-biljarts.be>
2015-03-27 14:30:55 +0100

    first commit

---
+++ MineField.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper1
+{
+
+    public class MineField
+    {
+        private int[] ValidDimentions = {81 /*Beginner*/, 256 /*Intermediate*/, 480 /*Expert*/};
+
+        private int dimentionValue;
+
+        public int Dimention
+        {
+            get { return dimentionValue; }
+            set 
+            {
+                if (ValidDimentions.Contains(value)) dimentionValue = value;
+                else throw new IndexOutOfRangeException("Given demention is not possible.");
+            }
+        }
+
+        public int Columns
+        {
+            get { return (int)Math.Sqrt(Dimention); }
+        }
+
+        public int Rows
+        {
+            get { return (int)Math.Sqrt(Dimention);  }
+        }
+
+        public int? Mines
+        {
+            get 
+            {
+                if (Dimention == ValidDimentions[0]) return 10;         // Beginner
+                else if (Dimention == ValidDimentions[1]) return 40;    // Intermediate
+                else if (Dimention == ValidDimentions[2]) return 99;    // Expert
+                else return null; 
+            }
+        }
+        
+
+        private List<MineSquare> squaresValue;
+
+        public List<MineSquare> Squares { get { return squaresValue; } set { squaresValue = value; } }
+
+        private void PopulateField()
+        {
+            List<int> mines = new List<int>();
+
+            Random MinePlacer = new Random();
+            int Mine;
+            for (int i = 0; i < Mines; i++)
+            {
+                do
+                {
+                    Mine = MinePlacer.Next(Dimention);
+                } while (mines.Contains(Mine));
+                mines.Add(Mine);
+            }
+
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    Squares.Add(new MineSquare(this, Rows, Columns, row, column));
+                    if (mines.Contains(Squares.Count - 1)) Squares[Squares.Count - 1].Mine = true;
+                }
+            }
+        }
+
+        public MineField(int dimention)
+        {
+            Dimention = dimention;
+            this.PopulateField();
+        }
+    }
+}
---
+++ MineSquare.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MineSweeper1
+{
+    public class MineSquare :  Button
+    {
+        enum SquareStatus
+        {
+            Cleared,
+            Mine,
+            Open,
+            Flagged,
+            Question,
+        }
+
+        private SquareStatus statusValue;
+
+        public SquareStatus Status
+        {
+            get { return statusValue; }
+            set { statusValue = value; }
+        }
+        
+
+        private int rowValue;
+
+        public int Row
+        {
+            get { return rowValue; }
+            set { rowValue = value; }
+        }
+
+        private int columnValue;
+
+        public int Column
+        {
+            get { return columnValue; }
+            set { columnValue = value; }
+        }
+
+        private int nrOfRowsValue;
+
+	    public int NrOfRows	
+        {
+		get { return nrOfRowsValue;}
+		set { nrOfRowsValue = value;}
+	    }
+
+        private int nrOfColumnsValue;
+
+        public int NrOfColumns
+        {
+            get { return nrOfRowsValue; }
+            set { nrOfRowsValue = value; }
+        }
+
+        private MineField fieldValue;
+
+        public MineField Field
+        {
+            get { return fieldValue; }
+            set { fieldValue = value; }
+        }
+
+        private bool mineValue;
+
+        public bool Mine
+        {
+            get { return mineValue; }
+            set { mineValue = value; }
+        }
+        
+
+        public int Mines
+        {
+            get
+            {
+                int mines = 0;
+                foreach (MineSquare Square in (List<MineSquare>)(Field.Squares.Where
+                    (s => (s.Row > this.Row-1) && (s.Row < this.Row+1) && 
+                    (s.Column > this.Column-1) && (s.Column < this.Column+1))
+                    .ToList()))
+                {
+                    if (Square.Mine == true) mines++;
+                }
+                return mines;
+            }
+        }
+        
+        public MineSquare(MineField field, int rows, int columns, int row, int column)
+        {
+            Field = field;
+            NrOfRows = rows;
+            NrOfColumns = columns;
+            Row = row;
+            Column = column;
+            Mine = false;
+            Status = SquareStatus.Open;
+        }
+	
+    }
+}
